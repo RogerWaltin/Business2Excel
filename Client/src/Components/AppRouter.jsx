8fd3dec4f12a647d9a82a685ed93c068ad3cff1c
Harris <harriswaltinjoy@gmail.com>
2026-06-06 20:29:39 +0530

    added community page

--- Client/src/Components/AppRouter.jsx
+++ Client/src/Components/AppRouter.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home/Home.jsx"
 import About from "../Pages/About/About.jsx"
 import Services from "../Pages/Services/Services.jsx"
 import Resources from "../Pages/Resources/Resources.jsx"
+import Community from "../Pages/Community/Community.jsx"
 
 export default function AppRouter() {
 
@@ -15,6 +16,7 @@ export default function AppRouter() {
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Services" element={<Services />} />
         <Route path="/Resources" element={<Resources />} />
+        <Route path="/Community" element={<Community />} />
       </Routes>
     </div>
   )
